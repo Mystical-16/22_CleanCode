5cb7460ff869178b5f008134a55f1e6372346ee7
Mystical-16 <89996866+Mystical-16@users.noreply.github.com>
2023-11-22 11:16:45 +0530

    Update Day1_M_Name.cs

--- Day1_M_Name.cs
+++ Day1_M_Name.cs
@@ -1,1 +1,86 @@
+review1.cs
+int d;//elapsed time in days 
+int elapsedTimeInDays;
 
+review2.cs
+var dataFromDb = db.GetFromService().ToList(); // Get List of employees 
+var EmployeedataFromDb =db.GetFromEmployeeService().toList();
+
+review3.cs
+int iCounter;
+string strFullName;
+DateTime dModifiedDate;
+
+int Counter;
+string FullName;
+DateTime ModifiedDate;
+
+review4.cs
+public bool IsShopOpen(string pDay, int pAmount)
+{
+    // some logic
+}
+
+public bool IsShopOpen(string Day, int Amount)
+{
+    // some logic
+}
+
+review5.cs
+
+const int DAYS_IN_WEEK = 7;
+const int daysInMonth = 30;
+
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var Artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+bool EraseDatabase() {}
+bool Restore_database() {}
+ changed
+class animal {}
+class Alpaca {}
+
+const int DaysInWeek = 7;
+const int DaysInMonth = 30;
+
+var Songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var Artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+bool EraseDatabase() {}
+bool RestoreDatabase() {}
+
+class Animal {}
+class Alpaca {}
+
+
+review6.cs
+
+public class Employee
+{
+    public Datetime sWorkDate { get; set; } // get set Start Working Date
+    public Datetime modTime { get; set; } // get set Modification Time
+}
+
+
+public class Employee
+{
+    public DateTime WorkDate { get; set; } // get set Start Working Date
+    public DateTime ModTime { get; set; } // get set Modification Time
+}
+
+review7.cs
+
+var employeephone;
+
+public double CalculateSalary(int workingdays, int workinghours)
+{
+    // some logic
+}
+
+var employeephone;
+
+public double CalculateSalary(int WorkingDays, int WorkingHours)
+{
+    // some logic
+}
+var EmployeePhone;
